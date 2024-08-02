8b0551e666ede7882655d8a57d6fd0c48d4ea5c1
Mayank Bansal <mayankbansal125@gmail.com>
2024-08-03 00:07:10 +0530

    feat: complete contact page

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,10 +1,22 @@
+import Link from "next/link";
+
 const Contact: React.FC = () => {
   return (
-    <div className='flex flex-col gap-12 mt-28 md:mt-32 max-w-5xl mx-auto'>
-      <h1 className='text-center text-2xl md:text-3xl lg:text-3xl text-dark-heading dark:text-light-heading font-semibold md:font-bold'>
-        if you wanna get in touch
-      </h1>
-      <a href='mailto:mayankbansl125@gmail.com'>mayankbansl125@gmail.com</a>
+    <div className="my-20 flex flex-col gap-3 justify-center items-center">
+      <div className="w-1/2">
+        <h2 className="text-3xl font-semibold mb-4">contact</h2>
+        <div className="flex flex-col gap-6 text-xl opacity-80">
+          <p>you can find me online on:</p>
+          <p>twitter: <Link href="https://x.com/SimplerMayank" className="font-semibold text-lg hover:underline hover:opacity-90 transition-all">simplermayank</Link> </p>
+          <p>github: <Link href="https://github.com/MayankBansal12" className="font-semibold text-lg hover:underline hover:opacity-90 transition-all">mayankbansal12</Link> </p>
+          <p>mail: <Link href="mailto:mayankbansal125@gmail.com" className="font-semibold text-lg hover:underline hover:opacity-90 transition-all">mayankbansal125@gmail.com</Link> </p>
+          <p>linkedin: <Link href="https://www.linkedin.com/in/mayank-bansal200604012/" className="font-semibold text-lg hover:underline hover:opacity-90 transition-all">--mb2004--</Link> </p>
+
+          <p>you can schedule a online meet using <Link href="https://cal.com/mayankbansal" className="font-semibold text-lg hover:underline hover:opacity-90 transition-all"> cal.com.</Link></p>
+          <p>my blog/newsletter (not super consistent): <Link href="https://cal.com/mayankbansal" className="font-semibold text-lg hover:underline hover:opacity-90 transition-all">substack_link</Link></p>
+          <p>if you wanna leave anonymous message or feedback, <Link href="https://mayank.sayout.net/" className="font-semibold text-lg hover:underline hover:opacity-90 transition-all">use this</Link> </p>
+        </div>
+      </div>
     </div>
   );
 };
