e575ecf53a720d5decc334f9c37c4d1ce14ea00d
Mayank Bansal <mayankbansal125@gmail.com>
2024-08-14 21:39:10 +0530

    add script for analytics

--- src/components/Metadata.tsx
+++ src/components/Metadata.tsx
@@ -8,6 +8,7 @@ const Metadata: React.FC = () => {
         name='description'
         content='mayank bansal portfolio, your friendly neighborhood developer. full stack developer'
       />
+      <script defer src="https://cloud.umami.is/script.js" data-website-id="1e889be1-5db9-4cc4-9453-f4d41ddfa46b"></script>
     </Head>
   );
 };
